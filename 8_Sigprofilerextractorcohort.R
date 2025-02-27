69ff68bba3f5a6e9e1dd0078db6e0993e7fa07d7
OncologyHNJ <bioinformaticaFIBHUNJ@gmail.com>
2025-02-27 15:12:17 +0100

    Update 8_Sigprofilerextractorcohort.R

--- 8_Sigprofilerextractorcohort.R
+++ 8_Sigprofilerextractorcohort.R
@@ -1,5 +1,12 @@
-#######CREATED BY:MARTA PORTASANY#################
-#############SIGPROFILEREXTRACTORCOHORT#################
+# ==================================================
+# Script: 8_Sigprofilerextractorcohort
+# Description: Execution of Sigprofilerextractor for the whole cohort
+# Author: Marta Portasany
+# Created on: 2025-02-27
+# Last modified: 2025-02-27
+# Pipeline: PULPO
+# Dependencies: reticulate, devtools, SigProfilerMatrixGeneratorR, SigProfilerExtractorR
+# ==================================================
 #########################################
 args <- commandArgs(trailingOnly = TRUE)
 inputdatacohort <- args[1]
@@ -13,14 +20,7 @@ library("SigProfilerMatrixGeneratorR")
 library("SigProfilerExtractorR")
 library("reticulate")
 library("devtools")
-###################################
-#inputdatacohort <- "/home/user/MARTA/PULPO_ejecutadoprueba/results/Cohort/SVs/Cohort.SV32.matrix.tsv"
-#pythondirectory <-"/home/user/miniconda3/envs/PULPO/bin/python3.10"
-#outputcohort <- "/home/user/MARTA/PULPO_ejecutadoprueba/results/Cohort/SVs/SigProfiler/"
-#minimum_signatures <- "1"
-#maximum_signatures <- "10"
-#nmf_replicates <- "5"
-###################################
+####################################
 use_python(pythondirectory)
 py_config()
 sigprofilerextractor("matrix", outputcohort, inputdatacohort, reference_genome = "GRCh38", opportunity_genome = "GRCh38", minimum_signatures = minimum_signatures, maximum_signatures = maximum_signatures, nmf_replicates = nmf_replicates, min_nmf_iterations = 1000, max_nmf_iterations =100000, nmf_test_conv = 1000, nmf_tolerance = 0.00000001)
